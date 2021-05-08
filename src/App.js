59ce0db667155540d417ddedae19f130598c8e77
Rakesh Ranjan <rakeshranjan.iiita@gmail.com>
2021-05-08 21:43:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+const api = {
+  key: "b85cbc154bc80d7deb76744dabd0a667",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+ 
+  }
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+             type="text"
+             className="search-bar"
+             placeholder="Search..."
+             />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box"> 
+          <div className="temp">
+            15
+          </div>
+
+      </div>
+        
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
